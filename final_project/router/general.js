4af17da0294858e35371852888b57b4080998c27
Solar <52679310+SolarTechCEO@users.noreply.github.com>
2024-09-06 11:31:47 -0500

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,13 +12,16 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.json({ books });
+  return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (req.params.isbn) {
+
+    } else {
+        return res.status(400).json({ message: "No ISBN Provided" })
+    }
  });
   
 // Get book details based on author
