cafdda05fb4427d66d47bc63fd7c83b1f4a614a3
Solar <52679310+SolarTechCEO@users.noreply.github.com>
2024-09-10 11:31:43 -0500

    asd

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,18 +5,26 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    if (req.body.username && req.body.password) {
+        if (!isValid(req.body.username)) {
+            users.push({ "username": req.body.username, "password": req.body.password })
+            return res.json({ message: "Successfully Created " + req.body.username })
+        } else {
+            return res.status(400).json({ message: "User Already Exists" })
+        }
+    } else {
+        return res.status(400).json({ message: "Username or Password Can Not Be Blank" })
+    }
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books));
+public_users.get('/', function (req, res) {
+    return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
     if (req.params.isbn) {
         if (books[req.params.isbn]) {
             return res.send(JSON.stringify(books[req.params.isbn]))
@@ -26,36 +34,51 @@ public_users.get('/isbn/:isbn',function (req, res) {
     } else {
         return res.status(400).json({ message: "No ISBN Provided" })
     }
- });
-  
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  let booksData;
+public_users.get('/author/:author', function (req, res) {
+    let booksData = [];
 
-  if (req.params.author) {
-    Object.entries(books).filter(book => book.forEach(b => { console.log(b.author) }));
-    booksData = Object.entries(books).filter(book => book.forEach(b => { b.author === req.params.author}));
-
-    if (booksData.length == 0) {
-        return res.status(400).json({ message: "No Books Found with Provided Author" })
+    if (req.params.author) {
+        Object.entries(books).filter(book => book.forEach(b => { if (b.author === req.params.author) booksData.push(b) }));
+        if (booksData.length == 0) {
+            return res.status(400).json({ message: "No Books Found with Provided Author" })
+        } else {
+            return res.send(JSON.stringify(booksData))
+        }
     } else {
-        return res.send(JSON.stringify(booksData))
+        return res.status(400).json({ message: "No Author Provided" })
     }
-  } else {
-    return res.status(400).json({ message: "No Author Provided" })
-  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    let booksData = [];
+
+    if (req.params.title) {
+        Object.entries(books).filter(book => book.forEach(b => { if (b.title === req.params.title) booksData.push(b) }));
+        if (booksData.length == 0) {
+            return res.status(400).json({ message: "No Books Found with Provided Title" })
+        } else {
+            return res.send(JSON.stringify(booksData))
+        }
+    } else {
+        return res.status(400).json({ message: "No Title Provided" })
+    }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    if (req.params.isbn) {
+        if (books[req.params.isbn]) {
+            return res.send(JSON.stringify(books[req.params.isbn].reviews))
+        } else {
+            return res.status(400).json({ message: "Invalid ISBN Provided" })
+        }
+    } else {
+        return res.status(400).json({ message: "No ISBN Provided" })
+    }
 });
 
 module.exports.general = public_users;
